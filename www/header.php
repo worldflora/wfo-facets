7b82af409e7e25bd0baa96a638731860ae4c880a
Roger Hyam <r.hyam@rbge.org.uk>
2024-04-26 17:21:05 +0100

    moved js

--- www/header.php
+++ www/header.php
@@ -16,7 +16,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <script src="javascript/main.js"></script>
+    <script src="js/main.js"></script>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
