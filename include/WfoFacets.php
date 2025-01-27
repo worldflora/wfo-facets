baee09dd07de2e9dbf79590fac03e983d5cd60bc
Roger <rhyam@rbge.org.uk>
2025-01-27 16:52:48 +0000

    greater than or equal to

--- include/WfoFacets.php
+++ include/WfoFacets.php
@@ -710,7 +710,7 @@ class WfoFacets{
 
         echo "Indexing Snippets modified since $modified_sql\n";
        
-        $sql = "SELECT * FROM `snippets` WHERE `modified` > '{$modified_sql}' AND meta_json is not null;"; // no need to order as we are not paging
+        $sql = "SELECT * FROM `snippets` WHERE `modified` >= '{$modified_sql}' AND meta_json is not null;"; // no need to order as we are not paging
         $response = $mysqli->query($sql, MYSQLI_USE_RESULT); // we allow for big result set
 
         echo $response->num_rows . "\t snippets to be indexed.\n";
